39c458cc58080891622e1c77eda35ec0dfea56f5
Karnika2604 <64522658+Karnika2604@users.noreply.github.com>
2023-04-09 01:18:02 +0530

    Update Extracting data from PDF.py

--- Extracting data from PDF.py
+++ Extracting data from PDF.py
@@ -9,23 +9,24 @@ import pdfquery
 
 pdf = PDFQuery('kt/SSRN-id3668857.pdf')
 pdf.load()
-
+  
 #convert the pdf to XML
 pdf.tree.write('kt/SSRN-id3668857.pdf.xml', pretty_print = True)
 pdf
 
+(#Result of above will show something similar as shown below)
+  
+ #<pdfquery.pdfquery.PDFQuery at 0x162d9e86fa0>)
+  
 
 # In[14]:
 
 
 # Use CSS-like selectors to locate the elements
 text_elements = pdf.pq('LTTextLineHorizontal').text()
-
-
 print(text_elements)
 
-
-# In[ ]:
+  
 
 
 
